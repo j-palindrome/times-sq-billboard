9d53adcf67695eaf890f9720958b182d3c65e9d0
Joshua Tazman Reinier <jreinier@oberlin.edu>
2024-03-20 16:12:00 -0400

    ugh

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 
 export const useEventListener = <K extends keyof WindowEventMap>(
   listener: K,
@@ -42,4 +42,26 @@ export const scale = <T extends number | number[]>(
   } else {
     return scaleNumber(input) as T
   }
+}
+
+export const create = <T>(e: T, onCreate: (argument: T) => void) => {
+  onCreate(e)
+  return e
+}
+
+export const useMemoCleanup = <T>(
+  create: () => T,
+  cleanup: (item: T) => void,
+  deps: any[] = []
+) => {
+  const itemRef = useRef<T | null>(null)
+  const item = useMemo(() => {
+    if (itemRef.current) {
+      cleanup(itemRef.current)
+      itemRef.current = null
+    }
+    return create()
+  }, deps)
+  itemRef.current = item
+  return item
 }
